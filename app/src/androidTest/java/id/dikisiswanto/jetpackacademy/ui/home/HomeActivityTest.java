c2328270786a09bca1754a3b9c49cd27998ed103
Diki Siswanto <dickyachmady@gmail.com>
2019-11-30 15:09:26 +0800

    Minor update UT on HomeActivity

--- app/src/androidTest/java/id/dikisiswanto/jetpackacademy/ui/home/HomeActivityTest.java
+++ app/src/androidTest/java/id/dikisiswanto/jetpackacademy/ui/home/HomeActivityTest.java
@@ -9,15 +9,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import id.dikisiswanto.jetpackacademy.R;
 import id.dikisiswanto.jetpackacademy.utils.EspressoIdlingResource;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.swipeLeft;
-import static androidx.test.espresso.action.ViewActions.swipeRight;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
@@ -31,19 +24,10 @@ public class HomeActivityTest {
 		activity = activityTestRule.getActivity();
 		IdlingRegistry.getInstance().register(EspressoIdlingResource.getEspressoIdlingResource());
 		activityTestRule.launchActivity(new Intent());
-		assertThat(activityTestRule, notNullValue());
 	}
 
 	@Test
-	public void swipePage() {
-		onView(withId(R.id.viewpager))
-				.check(matches(isDisplayed()));
-
-		onView(withId(R.id.viewpager))
-				.perform(swipeLeft());
-
-		onView(withId(R.id.viewpager))
-				.perform(swipeRight());
+	public void activityTest() {
+		assertThat(activityTestRule, notNullValue());
 	}
-
 }
